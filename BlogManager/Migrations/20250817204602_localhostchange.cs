39c919c604d11d46858e69d384eb45e1f0f60b2e
DESKTOP-TKS8BEI\Asus <Md. Mohaiminul Islam>
2025-09-04 00:11:18 -0300

    chore (backend): add database migration

---
+++ BlogManager/Migrations/20250817204602_localhostchange.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class localhostchange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
