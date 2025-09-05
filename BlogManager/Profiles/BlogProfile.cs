4f9e3e5b276d7d4a01c8eade022483768f46881c
DESKTOP-TKS8BEI\Asus <Md. Mohaiminul Islam>
2025-09-05 03:18:06 -0300

    feature (backend): modify: mapper

--- BlogManager/Profiles/BlogProfile.cs
+++ BlogManager/Profiles/BlogProfile.cs
@@ -10,15 +10,19 @@ namespace BlogManager.Profiles
         {
             // Entity -> DTO
             CreateMap<Post, PostDTO>()
-                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.DateCreated));
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.Name))
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.DateCreated))
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => src.DateModified));
 
             // DTO -> Entity
             CreateMap<CreatePostDTO, Post>()
-                .ForMember(dest => dest.DateCreated, opt => opt.MapFrom(src => DateTime.UtcNow));
+                .ForMember(dest => dest.DateCreated, opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForMember(dest => dest.DateModified, opt => opt.MapFrom(src => DateTime.UtcNow));
             CreateMap<UpdatePostDTO, Post>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
-                .ForMember(dest => dest.Author, opt => opt.Ignore())
-                .ForMember(dest => dest.DateCreated, opt => opt.Ignore());
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.DateCreated, opt => opt.Ignore())
+                .ForMember(dest => dest.DateModified, opt => opt.MapFrom(src => DateTime.UtcNow));
         }
     }
 }
